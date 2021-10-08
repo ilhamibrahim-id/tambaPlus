439512b6d858438bae6c4d4b8f86f7bc44555642
Ilham Ibrahim <ii.ilhamibrahim@gmail.com>
2021-10-08 09:36:16 +0700

    fixing blog dan kontak

--- resources/views/main/header.blade.php
+++ resources/views/main/header.blade.php
@@ -6,7 +6,7 @@
     <title>Tambah+</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" href="{{asset('images/logo3.png')}}" width="50px" height="50px">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <!-- Place favicon.ico in the root directory -->
 
