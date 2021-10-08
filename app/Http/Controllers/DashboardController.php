430adc46325e6ef0aa61e36f90ef44790fd39cb3
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-10-08 08:52:03 +0700

    Upadte controller dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
+use App\Models\Layanan;
 
 class DashboardController extends Controller
 {
-    //
+    public function blog(){
+        $data = Blog::all();
+        return view('blog', compact('data'));
+    }
+
+    public function layanan(){
+        $data = Layanan::all();
+        return view('servis', compact('data'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\DashboardController;
+use App\Models\Layanan;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/servis', function () {
-    return view('servis');
-});
+Route::get('/servis', [DashboardController::class, 'layanan']);
 Route::get('/kontak', function () {
     return view('kontak');
 });
