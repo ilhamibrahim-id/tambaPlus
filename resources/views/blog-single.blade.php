f4bfb5a9fb3a90e803df059abad48183af8739f0
Ilham Ibrahim <ii.ilhamibrahim@gmail.com>
2021-10-14 07:08:39 +0700

    error blog single

--- resources/views/blog-single.blade.php
+++ resources/views/blog-single.blade.php
@@ -9,39 +9,8 @@
       </div>
     </div>
   </section>
-  
-  
-   <!-- Navigation -section
-    =========================-->
-  <nav class="navbar navbar-fixed-top navigation" >
-    <div class="container">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand logo" href="index.html">
-          <img src="images/logo-yellow.png" alt="">
-        </a>
-      </div>
-  
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse navbar-ex1-collapse">
-        <ul class="nav navbar-nav navbar-right menu">
-          <li><a href="index.html">Home</a></li>
-          <li><a href="services.html">Service</a></li>
-          <li><a href="portfolio.html">Portfolio</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div>
-  </nav>
-    
-    <!-- Blog Sections 
+
+    <!-- Blog Sections
     =========================-->
     <section class="blog-single">
         <div class="container">
@@ -50,7 +19,7 @@
             <h2>Our Blog</h2>
           </div>
                 <div class="col-md-9">
-                    <!-- Blog Left Sections 
+                    <!-- Blog Left Sections
                     =========================-->
               <!-- Single Blog Page Main Img
               ============================== -->
@@ -140,7 +109,7 @@
             </div>
                 </div>
                 <div class="col-md-3">
-                    <!-- Blog Right Sections 
+                    <!-- Blog Right Sections
                     =========================-->
                         <div class="blog-sidbar">
           <div class="search widgets">
