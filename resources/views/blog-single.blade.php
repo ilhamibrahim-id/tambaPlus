59710a3ecb58d5fe76c015ea3d7376df1636010f
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-10-14 07:48:56 +0700

    update

--- resources/views/blog-single.blade.php
+++ resources/views/blog-single.blade.php
@@ -8,81 +8,6 @@
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
-      </div>
-    </div>
-  </section>
-
-    <!-- Blog Sections
-    =========================-->
-    <section class="blog-single">
-        <div class="container">
-            <div class="row">
-          <div class="title text-center">
-            <h2>Our Blog</h2>
-          </div>
-                <div class="col-md-9">
-                    <!-- Blog Left Sections
-                    =========================-->
-              <!-- Single Blog Page Main Img
-              ============================== -->
-              <div class="blog-single-section-img">
-                <img src="images/blog/single-blog.jpg" alt="Blog Single Img">
-              </div>
-              <!-- Single Blog Page Main Content
-              ================================== -->
-              <div class="blog-single-content">
-                <div class="blog-content-description">
-                  <h3><a class="blog-content-title" href="#">A Complete, Ranke Destinat Moines’ Good Bars,Benefit of the t Media Elite aliquip ex ea commodo .</a></h3>
-                  <div class="meta">
-                    <div class="date">
-                      <p>22<sup>nd</sup>Jan 2016</p>
-                    </div>
-                    <div class="author">
-                      <p>By Michal Lomans</p>
-                    </div>
-                  </div>
-                  <p class="blog-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. videntur parum clari, fiant sollemnes in futurum.</p>
-                </div>
-                <div class="blog-content-description">
-                  <h4 class="blog-inner-heading">Note The Basics</h4>
-                  <p class="blog-description">Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitu</p>
-                </div>
-                <div class="blog-content-description">
-                  <h4 class="blog-inner-heading">Selecting Color for Hunting or Target Shooting</h4>
-                  <p class="blog-description">r mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis  legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum.</p>
-                </div>
-              </div><!-- End Single Blog Content -->
-              <!-- Single Blog Page Comments
-              ================================== -->
-              <div class="blog-single-comments">
-                <h4 class="comments-title">Comments (2)</h4>
-                <div class="blog-comments">
-                  <div class="media">
-                    <div class="media-left blog-left-img">
-                      <div class="blog-comments-img">
-                        <img class="media-object" src="images/blog/comment-img1.png" alt="comments pic">
-                      </div>
-                    </div>
-                    <div class="media-body comments-body">
-                      <p class="blog-comments-text">“ This is the PERFECT conceal carry handgun..Only gave it a 4 because the slide is super hard to pull back but as I use it more I expect it to become easier. ” est etiam processus dynamicus</p>
-                      <h4 class="media-heading comments-author">Garry Shepherd</h4>
-                    </div>
-                  </div>
-                </div>
-                <div class="blog-comments">
-                  <div class="media">
-                    <div class="media-left blog-left-img">
-                      <div class="blog-comments-img">
-                        <img class="media-object" src="images/blog/comment-img2.png" alt="comments pic">
-                      </div>
-                    </div>
-                    <div class="media-body comments-body">
-                      <p class="blog-comments-text">“ This is the PERFECT conceal carry handgun..Only gave it a 4 because the slide is super hard to pull back but as I use it more I expect it to become easier. ” est etiam processus dynamicus</p>
-                      <h4 class="media-heading comments-author">Junaed Shepherd</h4>
-                    </div>
-                  </div>
-=======
     </section>
 
     <!-- Blog Sections 
@@ -92,74 +17,11 @@
             <div class="row">
                 <div class="title text-center">
                     <h2>{{ $data->judul }}</h2>
->>>>>>> 07d2800b558930076dd82c1aa1c3ad966d6c2131
                 </div>
                 <div class="col-md-12">
                     <div class="blog-single-section-img">
                         <img src="{{ asset('storage/' . $data->gambar) }}" alt="Blog Single Img">
                     </div>
-<<<<<<< HEAD
-                  </div>
-                </form>
-              </div>
-            <!-- See All Post -->
-            <div class="col-md-12">
-              <div class="see-all-post text-center">
-                <a class="btn btn-default th-btn solid-btn" href="#" role="button"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> Back To All Posts </a>
-              </div>
-            </div>
-                </div>
-                <div class="col-md-3">
-                    <!-- Blog Right Sections
-                    =========================-->
-                        <div class="blog-sidbar">
-          <div class="search widgets">
-              <form class="form-inline">
-                <div class="form-group search-input">
-                  <input type="text" class="form-control" placeholder="Search ...">
-                </div>
-                <button type="submit" class="btn btn-default tf-search-btn"><i class="tf-search"></i></button>
-              </form>
-          </div>
-          <div class="categories widgets">
-              <div class="list-group text-center">
-                  <div class="list-group-item active"> Blog Categories </div>
-                  <a href="#" class="list-group-item">Web Design</a>
-                  <a href="#" class="list-group-item">User Interface</a>
-                  <a href="#" class="list-group-item">User Experience</a>
-                  <a href="#" class="list-group-item">Typography</a>
-                  <a href="#" class="list-group-item">Color Sense</a>
-                  <a href="#" class="list-group-item">Future Trend</a>
-                  <a href="#" class="list-group-item">Modern Design</a>
-              </div>
-          </div>
-          <div class="related-post widgets">
-              <div class="list-group">
-                  <div class="list-group-item active text-center">
-                  Related Post
-                  </div>
-                  <a href="#" class="list-group-item">
-                      <div class="media">
-                        <div class="media-left media-middle"><p class="post-count">1</p></div>
-                        <div class="media-body">
-                          <p>Typography is  important fact for liusto odio dolore.</p>
-                        </div>
-                      </div>
-                  </a>
-                  <a href="#" class="list-group-item">
-                      <div class="media">
-                        <div class="media-left media-middle"><p class="post-count">2</p></div>
-                        <div class="media-body">
-                          <p>Typography is  important fact for liusto odio dolore.</p>
-                        </div>
-                      </div>
-                  </a>
-                  <a href="#" class="list-group-item">
-                      <div class="media">
-                        <div class="media-left media-middle"><p class="post-count">3</p></div>
-                        <div class="media-body">
-                          <p>Typography is  important fact for liusto odio dolore.</p>
-=======
                     <div class="blog-single-content">
                         <div class="blog-content-description">
                             <h3><a class="blog-content-title" href="#">{{ $data->judul }}</a></h3>
@@ -172,7 +34,6 @@
                                 </div>
                             </div>
                             <p class="blog-description">{{ $data->isi }}</p>
->>>>>>> 07d2800b558930076dd82c1aa1c3ad966d6c2131
                         </div>
                     </div>
                     <div class="col-md-12">
@@ -186,8 +47,4 @@
         </div>
     </section>
     @include('main.footer')
-<<<<<<< HEAD
-    @endsection
-=======
 @endsection
->>>>>>> 07d2800b558930076dd82c1aa1c3ad966d6c2131
